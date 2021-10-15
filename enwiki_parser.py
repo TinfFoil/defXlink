452ef08314b26175723e1425d8a45a215c705fdc
margherita-martinelli <martinellimargherita1997@gmail.com>
2021-10-15 12:32:09 +0200

    moved parsing and cleaning inside class

---
+++ enwiki_parser.py
@@ -0,0 +1,100 @@
+import xml.sax
+import subprocess
+import mwparserfromhell
+import re
+import os
+
+path = '/home/mmartinelli/persistent/en-wiki-food'
+data_path = r"/home/mmartinelli/project/corpora/wikidumps/enwiki-20210720-pages-articles-multistream.xml.bz2"
+
+# Function where ContentHandler looks for opening and closing tags title and text 
+# and adds characters enclosed within them to the buffer
+# content saved to a dict with tag as key
+
+class WikiXmlHandler(xml.sax.handler.ContentHandler):
+    """Content handler for Wiki XML data using SAX"""
+    def __init__(self):
+        xml.sax.handler.ContentHandler.__init__(self)
+        self._buffer = None
+        self._values = {}
+        self._current_tag = None
+        self._pages = []
+        self._counter = 0
+        self._flag = True
+        categories = ["Category:Italian cuisine", "Category:Cuisine of Abruzzo", 
+              "Category:Cuisine of Apulia", "Category:Cuisine of Basilicata", "Category:Cuisine of Calabria",
+              "Category:Cuisine of Campania", "Category:Cuisine of Emilia-Romagna", "Category:Cuisine of Lazio", 
+              "Category:Cuisine of Liguria", "Category:Cuisine of Lombardy", "Category:Cuisine of Marche",
+              "Category:Cuisine of Molise", "Category:Cuisine of Piedmond", "Category:Cuisine of Sardinia",
+              "Category:Cuisine of Sicily", "Category:Cuisine of South Tyrol", "Category:Cuisine of Tuscany",
+              "Category:Cuisine of Umbria", "Category:Cuisine of Veneto",
+              "Category:Cuisine of Aosta Valley", "Category:Dairy dishes", "Category:Egg dishes", 
+              "Category:Flower dishes", "Category:Fruit dishes", "Category:Ginger dishes", "Category:Grain dishes", 
+              "Category:Meat dishes", "Category:Mushroom dishes", "Category:Noodle dishes", "Category:Nut dishes", 
+              "Category:Pasta dishes", "Category:Tofu dishes", "Category:Tuber dishes", "Category:Vegetable dishes"]
+
+    def characters(self, content):
+        """Characters between opening and closing tags"""
+        if self._current_tag:
+            self._buffer.append(content)
+
+    def startElement(self, name, attrs):
+        """Opening tag of element"""
+        if name in ('title', 'id', 'text', 'timestamp'):         #do we need timestamp?
+            self._current_tag = name
+            self._buffer = []
+
+    def endElement(self, name):
+        """Closing tag of element"""
+        if name == self._current_tag:
+            #print(name, self._buffer)
+            if self._current_tag == "id": 
+                if self._flag:
+                    self._values[name] = ' '.join(self._buffer)
+                    self._flag = False
+            else:
+                self._values[name] = ' '.join(self._buffer)
+
+        if name == 'page':
+            self._flag = True
+            dumper(self._values['title'], self._values['id'], self._values['text'])
+            #self._pages.append((self._values['title'], self._values['id'], self._values['text']))
+            #print(self._pages[-1])
+    def dish_parser(self, title, myid, text):
+        texts = []
+        if any(cat in x[2] for cat in categories):
+            texts.append(x)
+            titles = [x[0] for x in texts]
+            parsed = [mwparserfromhell.parse(x[2]).strip_code().strip() for x in texts]         
+            parsed = [re.sub(r"(== See also == | ==See also== )\n *(.)*", "", el, flags=re.DOTALL) for el in parsed]
+            parsed = [re.sub(r"<[^>]+>", "", el) for el in parsed]
+            parsed = [re.sub(r"(  )*", "", el) for el in parsed]
+            parsed = ["\n".join(x) for x in zip(titles, parsed)]
+            
+            ids = myid[1]
+            article = parsed
+            file = '{}.txt'.format(ids)
+            with open(os.path.join(path, file), 'w') as f:
+                f.write('{}'.format(article))
+
+    
+# Object for handling xml
+handler = WikiXmlHandler()
+
+# Parsing object
+parser = xml.sax.make_parser()
+parser.setContentHandler(handler)
+
+
+counter = 0
+
+# Iterating through compressed file
+for i, line in enumerate(subprocess.Popen(['bzcat'], stdin = open(data_path), stdout = subprocess.PIPE).stdout):
+    
+    parser.feed(line)
+    
+#    if len(handler._pages) > 70000:
+#        break
+    counter += 1
+    if counter % 10000 == 0:
+        print("Current loop:", counter)
