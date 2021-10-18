e8b1700afa6b0388c25a8bed996e5e8a6f1b3404
margherita-martinelli <martinellimargherita1997@gmail.com>
2021-10-18 14:29:04 +0200

    modified code to fit into class

--- enwiki_parser.py
+++ enwiki_parser.py
@@ -21,7 +21,7 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
         self._pages = []
         self._counter = 0
         self._flag = True
-        categories = ["Category:Italian cuisine", "Category:Cuisine of Abruzzo", 
+        self.categories = ["Category:Italian cuisine", "Category:Cuisine of Abruzzo", 
               "Category:Cuisine of Apulia", "Category:Cuisine of Basilicata", "Category:Cuisine of Calabria",
               "Category:Cuisine of Campania", "Category:Cuisine of Emilia-Romagna", "Category:Cuisine of Lazio", 
               "Category:Cuisine of Liguria", "Category:Cuisine of Lombardy", "Category:Cuisine of Marche",
@@ -57,21 +57,22 @@ class WikiXmlHandler(xml.sax.handler.ContentHandler):
 
         if name == 'page':
             self._flag = True
-            dumper(self._values['title'], self._values['id'], self._values['text'])
+            self.dish_parser(self._values['title'], self._values['id'], self._values['text'])
             #self._pages.append((self._values['title'], self._values['id'], self._values['text']))
             #print(self._pages[-1])
     def dish_parser(self, title, myid, text):
         texts = []
-        if any(cat in x[2] for cat in categories):
-            texts.append(x)
-            titles = [x[0] for x in texts]
-            parsed = [mwparserfromhell.parse(x[2]).strip_code().strip() for x in texts]         
-            parsed = [re.sub(r"(== See also == | ==See also== )\n *(.)*", "", el, flags=re.DOTALL) for el in parsed]
-            parsed = [re.sub(r"<[^>]+>", "", el) for el in parsed]
-            parsed = [re.sub(r"(  )*", "", el) for el in parsed]
-            parsed = ["\n".join(x) for x in zip(titles, parsed)]
+        if any(cat in text for cat in categories):
+            texts.append(text)
+            #titles = [x[0] for x in texts]
+            parsed = mwparserfromhell.parse(text).strip_code().strip()         
+            parsed = re.sub(r"(== See also == | ==See also== )\n *(.)*", "", parsed, flags=re.DOTALL)
+            parsed = re.sub(r"<[^>]+>", "", parsed)
+            parsed = re.sub(r"(  )*", "", parsed)
+            parsed = "\n".join([title, parsed])
+            #parsed = ["\n".join(x) for x in zip(titles, parsed)]
             
-            ids = myid[1]
+            ids = myid
             article = parsed
             file = '{}.txt'.format(ids)
             with open(os.path.join(path, file), 'w') as f:
@@ -96,5 +97,5 @@ for i, line in enumerate(subprocess.Popen(['bzcat'], stdin = open(data_path), st
 #    if len(handler._pages) > 70000:
 #        break
     counter += 1
-    if counter % 10000 == 0:
+    if counter % 100000 == 0:
         print("Current loop:", counter)
